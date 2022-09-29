35e6d133907e99889d76cd9588048096c9303c1f
Niles <nileshs2398@gmail.com>
2022-09-29 13:01:26 +0530

    added searchAll functionality

---
+++ src/com/sms/exceptions/NullUserException.java
@@ -0,0 +1,17 @@
+package com.sms.exceptions;
+
+public class NullUserException extends Exception {
+
+	public NullUserException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public NullUserException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
